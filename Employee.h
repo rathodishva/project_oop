b04487024379a3b4999ceb2ff6fcd327f108b355
Ishva Rathod <ishvarathod@Ishvas-MBP.modem>
2023-10-20 06:01:05 +1030

    empl.h commenting

--- Employee.h
+++ Employee.h
@@ -4,14 +4,20 @@
 #include <string>
 
 class Employee {
-public:
+public: // public variables 
+   //  employee constructor which takes name and id of employee
     Employee(const std::string& name, int id);
+
+    // declaration to get the name of the employee.
     std::string get_name() const;
+
+    // declaration to get the ID of the employee.
     int get_id() const;
 
 private:
+    // private member variables to store the name and ID of the employee.
     std::string name;
     int id;
 };
 
-#endif // EMPLOYEE_H
+#endif 
