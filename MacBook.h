b968dd840bece9fe2fbf9c1792f51d7aab4a6013
Ishva Rathod <ishvarathod@Ishvas-MBP.modem>
2023-10-20 07:32:01 +1030

    macbook.h comments

--- MacBook.h
+++ MacBook.h
@@ -1,21 +1,21 @@
 #ifndef MACBOOK_H
 #define MACBOOK_H
 
-#include "iPad.h"
+#include "iPad.h"  // Include the iPad class header file to inherit.
 
 class MacBook : public iPad {
 private:
-    bool touch_bar;
-    bool mag_safe;
-    
+    bool touch_bar;  //   variable to store whether the MacBook has a touch bar.
+    bool mag_safe;   //   variable to store whether the MacBook has a MagSafe connector.
+
 public:
-    MacBook(); // Constructor
-    
-    bool check_touch_bar() const;
-    bool check_mag_safe() const;
-    
-    void set_touch_bar(bool val);
-    void set_mag_safe(bool val);
+    MacBook();  // Constructor for the MacBook class.
+
+    bool check_touch_bar() const;  //  to check if the MacBook has a touch bar.
+    bool check_mag_safe() const;  //   to check if the MacBook has a MagSafe connector.
+
+    void set_touch_bar(bool val);  //  function to set whether the MacBook has a touch bar.
+    void set_mag_safe(bool val);   //  function to set whether the MacBook has a MagSafe connector.
 };
 
-#endif // MACBOOK_H
+#endif 
