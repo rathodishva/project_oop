f55dbda7a36d5174ae2681bee68bb7efa99c3922
Ishva Rathod <ishvarathod@Ishvas-MBP.modem>
2023-10-19 17:47:17 +1030

    adjusted employ.h file

--- employee.h
+++ employee.h
@@ -1,19 +1,31 @@
-// employee.h
-
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
 #include <string>
+#include <vector>
 
 class Employee {
-public:
-    Employee(const std::string& name, int id);
-    std::string get_name() const;
-    int get_id() const;
-
 private:
     std::string name;
     int id;
+
+public:
+    // Default constructor
+    Employee();
+
+    // Constructor
+    Employee(const std::string& name, int id);
+
+    // Getters
+    std::string get_name() const;
+    int get_id() const;
+
+    // Setters
+    void set_name(const std::string& name);
+    void set_id(int id);
+
+    // Static function to initialize and return a list of employees
+    static std::vector<Employee> initializeEmployees();
 };
 
-#endif // EMPLOYEE_H
+#endif  // EMPLOYEE_H
