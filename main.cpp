9764f0155f383f552dcd4090051f86629a07ce0c
Ishva Rathod <ishvarathod@Ishvas-MBP.modem>
2023-10-20 08:04:04 +1030

    comment main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
-#include "Utils.h"
-#include "Employee.h"
+#include "Utils.h" // include utils.h to access employee login
+#include "Employee.h" // to ensure that employee funtions work
 
 int main() {
+    // main file used to idenitfy employees when first logging in 
+
+    //  Create a vector of employees.
 
     all_employees.push_back(Employee("Ishva Rathod", 1111));
     all_employees.push_back(Employee("Tirth Patel", 2222));
@@ -9,6 +12,6 @@ int main() {
     all_employees.push_back(Employee("User", 4444));
     all_employees.push_back(Employee("Joey Tribiani", 5555));
 
-    employeeLogin();
+    employeeLogin(); // Call the employee login function to handle user authentication.
     return 0;
 }
