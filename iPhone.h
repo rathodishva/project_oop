7f7bcffa949d36da8fec5fd8a7329d1367f81afd
Ishva Rathod <ishvarathod@Ishvas-MBP.modem>
2023-10-20 06:46:12 +1030

    comment iphone.h

--- iPhone.h
+++ iPhone.h
@@ -1,22 +1,27 @@
 #ifndef IPHONE_H
 #define IPHONE_H
 
-#include "Products.h" // Include the base class header file
+#include "Products.h" // Include the parent class header file
 
-class iPhone : public Products {
+class iPhone : public Products {  // create a child class from the parent class claled iphone
 public:
-    std::string model;
+    std::string model;  // sting varible to idenify model
     
-    iPhone(); // Constructor
+    iPhone(); // iphone constructor 
     
-    std::string get_model() const;
+    // get model for iphone class
+    std::string get_model() const; 
+
+    // set model for iphone class
     void set_model(const std::string& val);
     
+    // function to check if wireless charging is applicalbe for this phone
     bool check_wireless_charging() const;
     void set_wireless_charging(bool val);
     
 private:
+    // memeber variable for wireless charaging
     bool wireless_charging;
 };
 
-#endif // IPHONE_H
+#endif 
