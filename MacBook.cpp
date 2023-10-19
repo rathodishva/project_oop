3774e6322c89e38d5ac0ac67afe93f139b175e28
Ishva Rathod <ishvarathod@Ishvas-MBP.modem>
2023-10-20 07:40:49 +1030

    commented macbook.cpp

--- MacBook.cpp
+++ MacBook.cpp
@@ -1,22 +1,21 @@
-#include "MacBook.h"
+#include "MacBook.h"  // Include the MacBook class header file 
 
-// Constructor
+// Constructor for the MacBook class
 MacBook::MacBook() {
-    // Initialize the base class constructor if needed
 }
 
 bool MacBook::check_touch_bar() const {
-    return touch_bar;
+    return touch_bar;  //  function to check if the MacBook has a touch bar.
 }
 
 bool MacBook::check_mag_safe() const {
-    return mag_safe;
+    return mag_safe;  //  function to check if the MacBook has a MagSafe connector.
 }
 
 void MacBook::set_touch_bar(bool val) {
-    touch_bar = val;
+    touch_bar = val;  //  function to set whether the MacBook has a touch bar.
 }
 
 void MacBook::set_mag_safe(bool val) {
-    mag_safe = val;
+    mag_safe = val;  //  function to set whether the MacBook has a MagSafe connector.
 }
