e1797eece6dce51a449770a0ba19d16faf56c8b2
Ishva Rathod <ishvarathod@Ishvas-MBP.modem>
2023-10-19 18:56:30 +1030

    a

--- iphone.h
+++ iphone.h
@@ -1,19 +1,31 @@
 #ifndef IPHONE_H
 #define IPHONE_H
+#include <string>
 
-#include "Products.h" // Include the base class header
+#include "Products.h" // Include the parent class header
 
 class iPhone : public Products {
 public:
     iPhone(); // Constructor
+    std::string model;
+
+    // getter functioins 
     std::string get_model() const;
+
+    // setter function
     void set_model(const std::string& val);
-    bool check_wireless_charging() const;
-    void set_wireless_charging(bool val);
+    
 
 private:
-    std::string model;
+    
     bool wireless_charging;
+
+    // Getter functions
+    bool check_wireless_charging() const;
+
+    // Setter function for wireless charging
+    void set_wireless_charging(bool wireless_charging);
 };
 
 #endif
+
